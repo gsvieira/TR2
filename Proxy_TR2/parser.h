c4feb94b16f2f30f51042cb583ec25d30c623b39
SirLance14 <160009057@aluno.unb.br>
2019-12-05 05:44:47 -0300

    Update parser.h

--- Proxy_TR2/parser.h
+++ Proxy_TR2/parser.h
@@ -29,18 +29,15 @@
 #define REG_EMPTY_LINE "(\r?\n\r?\n)|$"
 
 /**
- * @brief The HttpParser class
- * This class implements functions to treat http request/responses
- * allowing gets some informations more easily, like host, port, encoding type,
- * encode an decode the body, set and get body header and the fully message.
+ Implementa funções que lidam com requisições e respostas HTTP para pegar informações mais facilmente (host, porta, tipo de codificação
+ , setar e pegar cabeçalho e a mensagem)
  */
 class HttpParser
 {
 public:
 
-    /*!
-     * \brief The content_types enum
-     * The kind of content: image/ json/ html / among others
+    /*
+    tipo de conteúdo são enumerados aki
      */
     enum content_types{
         IMAGE,
@@ -54,9 +51,8 @@ public:
         TYPE_NOT_FOUND
     };
 
-    /*!
-     * \brief The encoding_types enum
-     * The kind of encondign of content: gzip/ compress/ no encoded/ among others
+    /*
+     Tipos de codificações
      */
     enum encoding_types{
         NO_ENCODED,
@@ -70,9 +66,8 @@ public:
     };
 
 
-    /**
-     * \brief HttpParser
-     * Instantiates an object that permits handle with http messages more easily
+    /* 
+     * Instancia um objeto lida com mensagens HTTP
      */
     HttpParser();
     ~HttpParser();
@@ -80,41 +75,22 @@ public:
 
     /* Set methods */
 
-    /**
-     * @brief set_by_text
-     * Define the complete text of the message, and if in the header is define to be encoded
-     * this will be encoded as defined.
-     *
-     * @param text (char*) the text of the message
-     * @param text_size (unsigned long) the size of the text
-     * @return (bool) Returns true if the text has setted with success
+    /*
+     * Define o texto completo da mensagem e vai ser codificado de acordo com o cabeçalho da mensagem
      */
     bool set_by_text(char* text, unsigned long text_size);
 
-    /**
-     * @brief set_header
-     * Define only the header of the message.
-     *
-     * NOTE: if is passed a complete http text (has the empty line),
-     * him will be splitted at first empty line, getting only the header
-     *
-     * @param header (char*) the text of the header
-     * @param header_size (char*) the size of the header
-     * @return (bool) Returns true if the text has setted with success
+    /*
+     * Define o cabeçalho da mensagem
+     * Divide na primeira linha vazia pra pegar só o cabeçalho caso necessário
      */
     bool set_header(char *header, unsigned long header_size);
 
-    /**
-     * @brief set_body
-     * Define only the body of the message, and if in the header is define to be encoded
+
+     /* Define ocorpo da mensagem, codificado de acordo com o cabeçalho
      * this will be encoded as defined.
      *
-     * NOTE: if is passed a complete http text (there ar the start line of header),
-     * him will be splitted at first empty line, and getting only the body
-     *
-     * @param body (char*) the text of the body
-     * @param body_size (char*) size of body
-     * @return (bool) Returns true if the text has setted with success
+      * Divide na primeira linha vazia pra pegar só o corpo caso necessário
      */
     bool set_body(char* body, unsigned long body_size);
 
@@ -122,54 +98,34 @@ public:
     /* Get methods */
 
     /**
-     * @brief get_text
-     * Returns the complete text of the http
-     *
-     * @param encoded (bool, Default=false) if true returns the content encoded as header defined
-     * @return (char*) Returns the messsage of the http
+     * Retorna o texto HTTP completo
      */
     char* get_text(bool encoded=false);
 
-    /**
-     * @brief get_text_size
-     * Returns the size of the complete text of the http
-     *
-     * @return (unsigned long) Returns the size of the complete text of the http
+
+     /* Retorna o tamanho do texto HTTP
      */
     unsigned long get_text_size();
 
 
     /**
-     * @brief get_header
-     * Returns only the header of the text of the http
-     *
-     * @return (char*) Return the header
+     Retorna o cabeçalho da mensagem
      */
     char* get_header();
 
     /**
-     * @brief get_header_size
-     * Returns the size of the header of the http
-     *
-     * @return (unsigned long) Returns the size of the header of the http
+     * Retorna o tamanho do cabeçalho
      */
     unsigned long get_header_size();
 
 
     /**
-     * @brief get_body
-     * Returns only the body of the text of the http
-     *
-     * @param encoded (bool, Default=false) If true returns the content encoded as header defined
-     * @return (char*) Returns the body of the http or null if there are no
+     Retorna o corpo do texto
      */
     char* get_body(bool encoded=false);
 
     /**
-     * @brief get_body_size
-     * Returns the size of the body of the http
-     *
-     * @return (unsigned long) Returns the size of the body of the http
+      Retorna o tamanho do corpo do texto
      */
     unsigned long get_body_size();
 
@@ -177,77 +133,54 @@ public:
     /* Gets for especific informations */
 
     /**
-     * @brief get_port
-     * Returns the number of the port of the host in the header of the http
+     Retorna a porta do host no cabeçalho
      *
      * @return (u_int16_t) Returns the number of the host port or 80 (that is default port) if not found
      */
     u_int16_t get_port();
 
     /**
-     * @brief get_host
-     * Returns the name/ip of the host as described in the header of the http message
-     *
-     * @return (char*) Returns the name/ip of the host or nullptr if not found
+    Retorna o nome/ip do host descrito no cabeçalho
      */
     char* get_host();
 
-    /**
-     * @brief get_content_type
-     * Returns the Content-Type of the http message as described in the header
-     *
-     * @return (enum content_types*) Returns the enum indicating the type of the content or nullptr if not found
+    /*
+     * Retorna o tipo de conteúdo descrito no cabeçalho    
      */
     content_types get_content_type();
 
-    /**
-     * @brief get_encoding_type
-     * Returns the Content-Enconding / Transfer-Encoding types as described in the header of the http message
-     *
-     * @return (encoding_types*) Returns an Array with all the Enconding types defined in header
-     * or nullptr if not found
+
+     /* Retorna a codificação descrita no cabeçalho (content e transfer)
      */
     encoding_types* get_encoding_type();
 
-    /**
-     * @brief is_encoded
-     * Uses @ref get_encoding_type() function to verify if the header says something about encode
-     *
-     * @return returns true if header says something about encode in Content-Encoding or Transfer-Encoding
-     */
     bool is_encoded();
 
-    /**
-     * @brief is_readable
-     * Verify using @ref get_encoding_type() and @ref get_content_type() functions, if
-     * the content is not image or encoded
-     *
-     * @return Returns true if all message is readable (is not image or encoded)
-     */
+   
     bool is_readable();
 
 //private:
     /*!
      * \brief http_header
-     * The text of the header of the http message
+     * O texto do cabeçalho 
      */
     char* http_header = nullptr;
 
     /*!
      * \brief http_body
-     * The content of the http message
+     * O conteúdo do cabeçalho 
      */
     char* http_body = nullptr;
 
     /*!
      * \brief header_size
-     * The size of the header
+     * O tamanho do cabeçalho 
      */
     unsigned long header_size = 0;
 
     /*!
      * \brief body_size
-     * The size of the content
+     * O tamanho do conteúdo 
      */
     unsigned long body_size = 0;
 
