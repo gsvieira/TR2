f95401269103a4909931507ed6c6992684e2acc4
SirLance14 <160009057@aluno.unb.br>
2019-12-05 04:28:21 -0300

    Update connection.cpp

--- Proxy_TR2/connection.cpp
+++ Proxy_TR2/connection.cpp
@@ -4,12 +4,12 @@ Connection::Connection(int port)
 {
     Logger::write(Logger::INFO, "Connection", "Initializing Server");
 
-    // Define port seted to proxy server
+    // define a porta do proxy
     this->port_n = (uint16_t) port;
 
     Logger::write(Logger::INFO, "Connection", "Creating socket of server");
 
-    // Creating socket file descriptor
+    // cria descritor do socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         Logger::write(Logger::DANGER, "Connection", "Can't open socket<br>");
         connection_error = CANT_OPEN_SOCKET;
@@ -18,7 +18,7 @@ Connection::Connection(int port)
 
     Logger::write(Logger::INFO, "Connection", "Setting server socket options");
 
-    // Set the socket options
+    // seleciona opções do socket
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
         Logger::write(Logger::DANGER, "Connection", "Can't config socket<br>");
         connection_error = CANT_CONFIG_SOCKET;
@@ -27,7 +27,7 @@ Connection::Connection(int port)
 
     Logger::write(Logger::INFO, "Connection", "Setting server address");
 
-    // Set my address
+    // define endereço
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port_n);
@@ -36,7 +36,7 @@ Connection::Connection(int port)
 
     Logger::write(Logger::INFO, "Connection", "Forcing server to attach port: " + QString::number(port_n));
 
-    // Forcefully attaching socket to the selected port
+    //coloca socket na porta
     if (bind(server_fd, (struct sockaddr*) & address, sizeof(address)) < 0){
         Logger::write(Logger::DANGER, "Connection", "Bind failed<br>");
         connection_error = BIND_ERROR;
@@ -63,7 +63,7 @@ HttpParser* Connection::listen_browser()
 
     Logger::write(Logger::INFO, "Connection", "Waiting browser request");
 
-    // Listen browser in a blocked call
+    // escuta cliente
     if ((browser_socket = accept(server_fd, (struct sockaddr*) & address,
             (socklen_t*)& addrlen)) < 0) //Blocked until connect is called in the client
     {
@@ -72,10 +72,10 @@ HttpParser* Connection::listen_browser()
         return ret;
     }
 
-    // Read message from socket
+    // lê mensagem do socket
     ret = read_socket(browser_socket);
 
-    // Verify if socket was read with success
+    // verifica se leu socket
     if(connection_error != SUCCESS){
         return ret;
     }
@@ -90,7 +90,7 @@ void Connection::send_to_browser(HttpParser* buff_send)
 {
     Logger::write(Logger::INFO, "Connection", "Returning response to browser");
 
-    // Send to client (browser) the message (buffsend), and close socket
+    // manda mensagem pro cliente, e fecha socket
     send(browser_socket, buff_send->get_text(true), buff_send->get_text_size(), 0);
     close(browser_socket);
 
@@ -111,7 +111,7 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
     int client_fd = 0;
     HttpParser *ret = nullptr;
 
-    // Verify if host_server is not null
+    // Checa host_server pra ver se não é null
     if (host_server == nullptr){
         Logger::write(Logger::DANGER, "Connection", "No such host. Host is null");
         connection_error = NO_SUCH_HOST;
@@ -120,7 +120,7 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
 
     Logger::write(Logger::INFO, "Connection", "Creating socket of client");
 
-    // Creating socket file descriptor to communicate with external server
+    // Criando descritor do socket
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         Logger::write(Logger::DANGER, "Connection", "Can't open socket<br>");
         connection_error = CANT_OPEN_SOCKET;
@@ -129,7 +129,7 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
 
     Logger::write(Logger::INFO, "Connection", "Getting ip host by name");
 
-    // Get ip of host by host_name, and verify if is valid
+    // Pega IP do host pelo host_name, verifica se é valido
     server_address = gethostbyname(host_server);
     if (server_address == nullptr){
         Logger::write(Logger::DANGER, "Connection", "No such host: " + QString(host_server) + "<br>");
@@ -140,7 +140,7 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
 
     Logger::write(Logger::INFO, "Connection", "Setting server address");
 
-    // Set server address
+    // Endereço do servidor
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
@@ -150,7 +150,7 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
 
     Logger::write(Logger::INFO, "Connection", "Traing to connect with server");
 
-    // Try to connect
+    // Conecta
     if (connect(client_fd, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) < 0){
         Logger::write(Logger::DANGER, "Connection", "Can't connect<br>");
         connection_error = ERROR_CONNECTING;
@@ -162,21 +162,21 @@ HttpParser* Connection::send_to_server(const char* host_server, uint16_t port, H
 
     Logger::write(Logger::INFO, "Connection", "Sending request to server");
 
-    // Send to external server the request (buff_send)
+    // Manda requisição para servidor externo
     send(client_fd, buff_send->get_text(true), buff_send->get_text_size(), 0);
 
     Logger::write(Logger::INFO, "Connection", "Getting response from server");
 
-    // Read response sended by external server
+    // Lê resposta do servidor externo
     ret = read_socket(client_fd);
 
-    // Verify if message was read with success
+    // vê se a mensagem foi lida
     if(connection_error != SUCCESS){
         close(client_fd);
         return ret;
     }
 
-    // close socket
+    // fecha o socket
     close(client_fd);
 
     Logger::write(Logger::SUCCESS, "Connection", "Server response something<br>");
@@ -196,15 +196,15 @@ char* Connection::resize_char(char*& old,unsigned long length,unsigned long resi
     new_ptr = new char[ resize_to ];
     unsigned long least = ( length < resize_to ) ? length : resize_to;
 
-    // Copy old char array to new char array
+    //copia vetor
     for(unsigned long i = 0;i < least ; ++i)
         new_ptr [i] = old[i];
 
-    // Fill with '\0' empty spaces in new char array
+    // enche de '\0' no lugar de espaços vazios
     for(unsigned long i=least; i< resize_to; ++i)
         new_ptr [i] = 0;
 
-    // Delete old char array
+    // deleta vetor que não é mais usado
     delete [] old;
     old = nullptr;
 
@@ -219,15 +219,15 @@ Connection::errors Connection::wait_response(int fd){
     tv.tv_sec = 1;
     tv.tv_usec = 250000;
 
-    // Reset map of file descriptors, and set docket (fd) to be watched
+    // Reseta mapa de descritores, e observa socket (fd) 
     // http://beej.us/guide/bgnet/html/single/bgnet.html#select
     FD_ZERO(&readfds);
     FD_SET(fd, &readfds);
 
-    // Watche socket until receive something or get timeout
+    // Observa socket até chegar algo ou dar timeout
     select(fd+1, &readfds, nullptr, nullptr, &tv);
 
-    // Verify if select returns because have something to read, or was get timeout
+    // Verifica se select retorna pq tem algo pra ler ou pq deu timeout
     if(FD_ISSET(fd, &readfds)){
         return SUCCESS;
     }
@@ -244,20 +244,20 @@ HttpParser* Connection::read_socket(int fd){
 
     connection_error = SUCCESS;
 
-    // Read all message in socket (fd)
+    // Lê tudo no socket (fd)
     while(true){
-        // Wait until have something to read
+        // Espera até ter algo pra ler
         if(wait_response(fd) != SUCCESS){
             break;
         }
 
-        buffer_size = buffer_cursor + 2048;                            // Define the buffer size = acctual size + 2048 (max that can be read in one call)
-        buffer = resize_char(buffer, buffer_size - 2048, buffer_size); // Resize  buffer to new buffer size = actual size + 2048
-        valread = read(fd, (buffer + buffer_cursor), 2048);            // Read message and save starting at cursor point
-        buffer_cursor += valread;                                      // Set cursor/actual size = last size + read size
+        buffer_size = buffer_cursor + 2048;                            // Redefine o tamanho do buffer para o tamanho máximo de leitura
+        buffer = resize_char(buffer, buffer_size - 2048, buffer_size); // Redefine o buffer para o novo tamanho de buffer = tamanho atual + 2048
+        valread = read(fd, (buffer + buffer_cursor), 2048);            // Lê mensagem e salva e salva no cursor
+        buffer_cursor += valread;                                      // Cursor = tamanho da leitura + último tamanho
 
-        // Verify if read is not 0, that is one indicator that is an https connection
-        // valread == 0 not necessary means that is a https, but commonly is
+        // verifica se read não é 0, indica se é uma conexão HTTPS
+        // valread == 0 pode ser https
         if(valread == 0){
            Logger::write(Logger::DANGER, "Connection", "HTTPS connection identyfied, closing<br>");
            connection_error = HTTPS;
@@ -265,10 +265,10 @@ HttpParser* Connection::read_socket(int fd){
         }
     }
 
-    // Resize buffer to be exacly the size of read message
+    // Buffer tem o tamanho da mensagem
     buffer = resize_char(buffer, buffer_cursor, buffer_size);
 
-    // Set return: data and data_size
+    // return: data and data_size
     ret->set_by_text(buffer, buffer_cursor);
 
     return ret;
