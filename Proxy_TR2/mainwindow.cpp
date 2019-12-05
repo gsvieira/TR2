9c5297f500f405410e6c1a3ecc1eec1a361f79fa
SirLance14 <160009057@aluno.unb.br>
2019-12-05 04:59:57 -0300

    Update mainwindow.cpp

--- Proxy_TR2/mainwindow.cpp
+++ Proxy_TR2/mainwindow.cpp
@@ -16,23 +16,23 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    int port = ui->lineEdit->text().split(" ")[0].toInt();  // Gets input port value
-    proxy_server = ProxyServer::get_instance(port);         // Gets the instance of singleton ProxyServer
-    proxy_manager_window = new ProxyManager(this);          // Create new window to show proxy manager
-    proxy_manager_window->showMaximized();                  // and show
-    logger_window = Logger::get_instance(this);             // Gets the instance of singleton Logger
-    this->hide();                                           // Hide this window
+    int port = ui->lineEdit->text().split(" ")[0].toInt();  // Recebe valor da porta
+    proxy_server = ProxyServer::get_instance(port);         // recebe instância do singleton ProxyServer
+    proxy_manager_window = new ProxyManager(this);          // Cria uma nova janela para o proxy
+    proxy_manager_window->showMaximized();                  //
+    logger_window = Logger::get_instance(this);             // Recebe instância do singleton Logger
+    this->hide();                                           // Esconde janela
 
-    proxy_server->startServer();                            // Initialize proxy server
+    proxy_server->startServer();                            // inicializa o proxy
 }
 
 void MainWindow::back()
 {
-    proxy_server->stopServer();                             // Stop proxy server
-    proxy_manager_window->deleteLater();                    // Delete view of proxy window
-    logger_window->deleteLater();                           // Delete logger dialog
-    this->show();                                           // And show again the main window
+    proxy_server->stopServer();                             // para o proxy 
+    proxy_manager_window->deleteLater();                    // Deleta a view do proxy
+    logger_window->deleteLater();                           // Deleta logger dialog
+    this->show();                                           // Mostra janela principal
 
-    // NOTE: stopServer isn't instantly if you start again and last haven't finished yet
-    // proxy manage will show the last server again. This can cause some bugs
+    // NOTE: stopServer não é instantâneo
+    //fechar e abrir gera bugs
 }
